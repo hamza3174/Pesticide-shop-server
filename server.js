ce2b39058fcb41be082eb450b4e456e7db34c559
Hamza Siddique <mhamza31745@gmail.com>
2026-06-07 12:06:07 +0500

    Fix cors

--- server.js
+++ server.js
@@ -16,7 +16,21 @@ const app = express();
 
 // ================= MIDDLEWARE =================
 app.use(express.json());
-app.use(cors());
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: [
+            "https://pesticide-shop-client.vercel.app",
+            "http://localhost:5173",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+app.options("*", cors());
 
 // ================= ENV =================
 const PORT = process.env.PORT || 3001;
